45ef6687409a61228487de0e3fd275ca7950b493
ye yu jin <hina0510@naver.com>
2023-06-22 14:36:28 +0900

    정수기 초기 설정

---
+++ src/water_purifier_main/MainClass.java
@@ -0,0 +1,24 @@
+package water_purifier_main;
+
+import java.util.Scanner;
+
+public class MainClass {
+public static void main(String[] args) {
+	Scanner input = new Scanner(System.in);
+	while(true) {
+		int num;
+		System.out.println("1. 온도 조절");
+		System.out.println("2. 필터 오염도 확인");
+		System.out.println("3. 얼음 잔량 확인");
+		System.out.println("4. 전체 설정 확인");
+		System.out.print(">>>");
+		num = input.nextInt();
+		switch (num) {
+		case 1:break;
+		case 2:break;
+		case 3:break;
+		case 4:break;
+		}
+	}
+}
+}
