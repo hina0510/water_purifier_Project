85d7d43543080b3e2748bcb47766716549b623be
ye yu jin <hina0510@naver.com>
2023-06-22 15:15:29 +0900

    얼음기능

---
+++ src/ice/IceClass.java
@@ -0,0 +1,36 @@
+package ice;
+
+import java.util.Scanner;
+
+public class IceClass {
+	Scanner input = new Scanner(System.in);
+	public int ice=0, su=0;
+	public void createIce() {
+		if(ice>=0 && ice<=50) {
+			if(ice>40) {
+				System.out.println("얼음이 가득찼습니다(최대 50개)");
+			}else {
+				System.out.println("얼음을 10개 추가합니다");
+				ice +=10;
+			}
+		}
+	}
+	public void putIce() {
+		System.out.print("추출할 얼음 수 : ");
+		su = input.nextInt();
+		if(su>ice) {
+			System.out.println("얼음이 부족합니다");
+		}else {
+			System.out.println(ice+"개 추출합니다");
+			ice -= su;
+		}
+	}
+	public void printIce() {
+		if(ice==0) {
+			System.out.println("얼음이 없습니다");
+		}else {
+			System.out.println("현재 얼음 갯수 : "+ice);
+		}
+		
+	}
+}
--- src/water_purifier_main/MainClass.java
+++ src/water_purifier_main/MainClass.java
@@ -2,11 +2,14 @@ package water_purifier_main;
 
 import java.util.Scanner;
 
+import ice.IceClass;
+
 public class MainClass {
 public static void main(String[] args) {
 	Scanner input = new Scanner(System.in);
+	IceClass ice = new IceClass();
 	while(true) {
-		int num;
+		int num, num1;
 		System.out.println("1. 온도 조절");
 		System.out.println("2. 필터 오염도 확인");
 		System.out.println("3. 얼음 잔량 확인");
@@ -16,7 +19,25 @@ public static void main(String[] args) {
 		switch (num) {
 		case 1:break;
 		case 2:break;
-		case 3:break;
+		case 3:
+			while(true) {
+				System.out.println("1. 얼음 생성");
+				System.out.println("2. 얼음 추출");
+				System.out.println("3. 얼음 잔량 확인");
+				System.out.println("4. 나가기");
+				System.out.print(">>>");
+				num1 = input.nextInt();
+				if(num1 == 1) {
+					ice.createIce();
+				}else if(num1 == 2) {
+					ice.putIce();
+				}else if(num1 == 3) {
+					ice.printIce();
+				}else if(num1 == 4) {
+					break;
+				}
+			}
+			break;
 		case 4:break;
 		}
 	}
