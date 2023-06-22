e337377da3bd9e7584ffbafacf03e8a2c04d5878
choi <a@a.com>
2023-06-22 15:08:20 +0900

    filter기능추가

--- src/water_purifier_main/MainClass.java
+++ src/water_purifier_main/MainClass.java
@@ -2,6 +2,8 @@ package water_purifier_main;
 
 import java.util.Scanner;
 
+import mingdid1.filter;
+
 public class MainClass {
 public static void main(String[] args) {
 	Scanner input = new Scanner(System.in);
@@ -15,7 +17,10 @@ public static void main(String[] args) {
 		num = input.nextInt();
 		switch (num) {
 		case 1:break;
-		case 2:break;
+		case 2:
+			filter f = new filter();
+			f.pollution();
+			break;
 		case 3:break;
 		case 4:break;
 		}
